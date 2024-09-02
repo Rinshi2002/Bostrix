6e4f068fa1f93da8f7453fd6a2bd5bf813565b8f
rinshida <rinshidak2002@gmail.com>
2024-09-02 21:44:18 +0530

    Bug Fixed

---
+++ max.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int a = 7;
+  int b = 3;
+  if (a > b){
+    cout << "maximum number is: " << a;
+  }
+  else {
+     cout << "maximum number is: " << a;
+     }
+     return 0;
+    }
