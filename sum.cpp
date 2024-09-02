7fe6cd229b34271c2002768a6b113634de1e3468
Rinshida <98110646+Rinshi2002@users.noreply.github.com>
2024-09-02 16:32:53 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int a,b,c;
+  cout << "Type first number:";
+  cin >> a;
+  cout << "Type second number:";
+  cin >> b;
+  c = a + b;
+  cout << "Sum of a and b is: " << c;
+  return 0;
+  }
